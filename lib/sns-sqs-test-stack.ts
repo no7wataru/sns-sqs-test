df9380af9b7da11ac93750e7192662e617a36763
Wataru Yoshioka <no7.wataru@gmail.com>
2023-12-07 19:28:17 +0900

    Define resources

--- lib/sns-sqs-test-stack.ts
+++ lib/sns-sqs-test-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class SnsSqsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const topic = new sns.Topic(this, 'payload-key-topic', {
+      topicName: 'payload-key-topic',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SnsSqsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queueV1 = new sqs.Queue(this, 'payload-v1-key-queue', {
+      queueName: 'payload-v1-key-queue',
+    });
+
+    const queueV2 = new sqs.Queue(this, 'payload-v2-key-queue', {
+      queueName: 'payload-v2-key-queue',
+    });
+
+    topic.addSubscription(new subscriptions.SqsSubscription(queueV1, {
+      filterPolicy: {
+        version: sns.SubscriptionFilter.numericFilter({
+          allowlist: [1],
+        }),
+      },
+    }));
+
+    topic.addSubscription(new subscriptions.SqsSubscription(queueV2, {
+      filterPolicy: {
+        version: sns.SubscriptionFilter.numericFilter({
+          allowlist: [2],
+        }),
+      },
+    }));
   }
 }
