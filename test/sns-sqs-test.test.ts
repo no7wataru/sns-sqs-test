3697d55d74d788bbbed78e758b03f73d4a44cded
Wataru Yoshioka <no7.wataru@gmail.com>
2023-12-07 17:31:07 +0900

    Initial commit

---
+++ test/sns-sqs-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SnsSqsTest from '../lib/sns-sqs-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sns-sqs-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SnsSqsTest.SnsSqsTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
